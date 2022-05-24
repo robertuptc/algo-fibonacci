4c81ce2e655d56d219ba612520ff4922ec0465e7
robertuptc <robertuptc@gmail.com>
2022-05-24 19:18:16 -0400

    finished exercise

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (num) => {
+    let fibonacciArr = [0, 1,];
     
+    if (num === 0) {
+        return 0
+    } else if (num === 1) {
+        return 1
+    } else {
+        for(let i = 2; i <= num; i++) {
+            fibonacciArr.push(fibonacciArr[i-1] + (fibonacciArr[i-2]))    } fibonacciArr
+    } return fibonacciArr[num]  
 }
 
 module.exports = {fibonacci}
